c109c12617f3c1e03cf0ac35a6ed82df5a93a249
Jaypee <eyibrajohnpaul@gmail.com>
2022-11-08 09:19:48 +0100

    feat: override esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    process.env.MODE !== 'production'
+    ? react({
+        jsxRuntime: 'classic',
+      })
+    : react(),
+  ]
 })
